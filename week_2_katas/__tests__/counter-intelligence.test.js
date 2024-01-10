ae4c4e1f54a0ab8b6378b1e1d3d05a70d5a96b4e
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-01-10 17:37:07 +0000

    Complete kata 1 - Counter Intelligence

--- week_2_katas/__tests__/counter-intelligence.test.js
+++ week_2_katas/__tests__/counter-intelligence.test.js
@@ -1,7 +1,26 @@
 const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
-  it('', () => {
-    // first test here
+  it('Should return string', () => { 
+    expect(typeof counterIntelligence()).toBe('string');
+  });
+  it('Argument of "Y" should return "X"', () => { 
+    expect(counterIntelligence('Y')).toBe('X');
+  });
+  it('Argument of "BCD Y" should return "ABC X"', () => { 
+    expect(counterIntelligence('BCD Y')).toBe('ABC X');
+  });
+  it('Argument of "NKRRU YCKKZNKGXZ D" should return "HELLO SWEETHEART X"', () => { 
+    expect(counterIntelligence('NKRRU YCKKZNKGXZ D')).toBe('HELLO SWEETHEART X');
+  });
+  it('Whole sentence with multiple spaces, commas should return decoded sentence', () => { 
+    const input = 'ANVNVKNA CX YRLT DY IDLLQRWR XW HXDA FJH QXVN OAXV FXAT, MJAURWP G';
+    const expectedOutput = 'REMEMBER TO PICK UP ZUCCHINI ON YOUR WAY HOME FROM WORK, DARLING X';
+    expect(counterIntelligence(input)).toBe(expectedOutput);
+  });
+  it('Whole sentence with multiple spaces, commas should return decoded sentence', () => { 
+    const input = 'ngbk g toik jge :) d';
+    const expectedOutput = 'HAVE A NICE DAY :) X';
+    expect(counterIntelligence(input)).toBe(expectedOutput);
   });
 });
--- week_2_katas/counter-intelligence.js
+++ week_2_katas/counter-intelligence.js
@@ -1,6 +1,23 @@
 // Please do not change the name of this function
 const counterIntelligence = (str) => {
   // Your code here
+  if (!str) return '';
+  str = str.toLowerCase();
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  const decoder = (letter) => {
+    let difNum = alphabet.indexOf('x')-alphabet.indexOf(str[str.length - 1]);
+    let total = alphabet.indexOf(letter) + difNum;
+    return alphabet[total%alphabet.length];
+  }
+
+  const newString = str.split("").map((letter)=>{
+    if (/[^a-zA-Z]/g.test(letter)) return letter;
+    return decoder(letter).toUpperCase();
+  });
+
+  return newString.join("");
 };
 
 module.exports = { counterIntelligence };
+
