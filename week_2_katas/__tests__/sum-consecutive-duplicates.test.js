474f3d9eefff360a4b0663f101fd4498251654a9
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-01-17 19:54:19 +0000

    Complete kata 3 - Sum Consecutive Duplicates

--- week_2_katas/__tests__/sum-consecutive-duplicates.test.js
+++ week_2_katas/__tests__/sum-consecutive-duplicates.test.js
@@ -1,7 +1,23 @@
-const { sumConsecutiveDuplicates } = require('../sum-consecutive-duplicates');
+const { sumConsecutiveDuplicates, reduceConsecutives } = require('../sum-consecutive-duplicates');
 
 describe('sumConsecutiveDuplicates()', () => {
-  it('', () => {
+  it('3 consecutive numbers not consecutive duplicate should return the same input numbers', () => {
     // first test here
+    expect(sumConsecutiveDuplicates([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+  it('1 consecutive duplicate number should return array with consecutive duplicates added', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 3])).toEqual([2, 2, 3]);
+  });
+  it('2 consecutive duplicate number should return array with consecutive duplicates added', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 2, 3, 3])).toEqual([2, 4, 6]);
+  });
+  it('3 consecutive duplicate number should return array with consecutive duplicates added', () => {
+    expect(sumConsecutiveDuplicates([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1])).toEqual([2, 2, 4, 2, 3]);
+  });
+});
+
+describe('sumConsecutiveDuplicates()', () => {
+  it('reduces array until there are no more consecutive duplicates', () => {
+    expect(reduceConsecutives([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1])).toEqual([8, 2, 3]);
   });
 });
--- week_2_katas/sum-consecutive-duplicates.js
+++ week_2_katas/sum-consecutive-duplicates.js
@@ -1,6 +1,25 @@
 // Please do not change the name of this function
 const sumConsecutiveDuplicates = (arr) => {
   // Your code here
+  const newArr = [];
+  
+  arr.forEach((num,i,all)=>{
+    if(all[i-1]){
+      num === all[i-1] ? newArr[newArr.length-1] += num : newArr.push(num);
+    } else {
+      newArr.push(num);
+    }
+  });
+
+  return newArr;
 };
 
-module.exports = { sumConsecutiveDuplicates };
+const reduceConsecutives = (arr) => {
+  if (arr.toString() === sumConsecutiveDuplicates(arr).toString()){
+    return arr;
+  } else if (arr.toString() !== sumConsecutiveDuplicates(arr).toString()){
+    return reduceConsecutives(sumConsecutiveDuplicates(arr));
+  }
+}
+
+module.exports = { sumConsecutiveDuplicates, reduceConsecutives };
