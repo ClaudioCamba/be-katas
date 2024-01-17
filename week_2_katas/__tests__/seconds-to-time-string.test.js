ec3ad1a5a439dc9ee1c9625b77c9d445281dab67
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-01-17 18:44:46 +0000

    Complete kata 2 - Seconds to Time String Sentence

--- week_2_katas/__tests__/seconds-to-time-string.test.js
+++ week_2_katas/__tests__/seconds-to-time-string.test.js
@@ -12,6 +12,18 @@ describe('secondsToTimeString()', () => {
     expect(secondsToTimeString(60)).toBe('1 minute');
   });
   it('Input of 121 should return "2 minutes and 1 second"', () => {
+    expect(secondsToTimeString(120)).toBe('2 minutes');
+  });
+  it('Input of 121 should return "2 minutes and 1 second"', () => {
     expect(secondsToTimeString(121)).toBe('2 minutes and 1 second');
+  });
+  it('Input of 3661 should return "1 hour, 1 minute and 1 second"', () => {
+    expect(secondsToTimeString(3661)).toBe('1 hour, 1 minute and 1 second');
+  });
+  it('Input of 31626061 should return "1 year, 1 day, 1 hour, 1 minute and 1 second"', () => {
+    expect(secondsToTimeString(31626061)).toBe('1 year, 1 day, 1 hour, 1 minute and 1 second');
+  });
+  it('Input of 31626061 should return "4 years, 18 days, 1 hour, 2 minutes and 22 seconds"', () => {
+    expect(secondsToTimeString(127702942)).toBe('4 years, 18 days, 1 hour, 2 minutes and 22 seconds');
   });
 });
--- week_2_katas/seconds-to-time-string.js
+++ week_2_katas/seconds-to-time-string.js
@@ -1,14 +1,45 @@
 // Please do not change the name of this function
 const secondsToTimeString = (seconds) => {
   // Your code here
-  let time;
-  if (seconds < 60){
-    time = seconds > 1 ? seconds + ' seconds' : seconds + ' second';
-  }else {
-    let mins = seconds / 60;
-    time = mins > 1 ? mins + ' minutes' : mins + ' minute';
-    
+  let time = '';
+
+  const duration = {
+    'seconds': null,
+    'minutes': null,
+    'hours': null,
+    'days': null,
+    'years': null
   }
+
+  while (seconds > 0) {
+    if (seconds >= 31536000){
+      seconds -= 31536000;
+      duration.years += 1;
+    } else if (seconds >= 86400){
+      seconds -= 86400;
+      duration.days += 1;
+    } else if (seconds >= 3600){
+      seconds -= 3600;
+      duration.hours += 1;
+    } else if (seconds >= 60){
+      seconds -= 60;
+      duration.minutes += 1;
+    } else if (seconds >= 1){
+      seconds -= 1;
+      duration.seconds += 1;
+    }
+  }
+
+  ['years','days','hours','minutes','seconds'].forEach((elem,i,arr) => {
+    if (duration[elem]){
+      if (duration[arr[i-1]]){
+        elem === 'seconds'? time += ` and ` : time += `, `;
+      }
+      duration[elem] > 1 ? time += `${duration[elem]} ${elem}` : time += `${duration[elem]} ${elem.substring(0,elem.length-1)}`;
+    }
+  });
+
+
   return time;
 };
 
