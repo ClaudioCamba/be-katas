adbee28a4d95b281d1f76c4e7d575b0cd22ee0e6
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-07 13:43:36 +0100

    Backend week 1 treat-doggos kata solution

--- week_1_katas/__tests__/treat-doggos.test.js
+++ week_1_katas/__tests__/treat-doggos.test.js
@@ -1,7 +1,16 @@
 const treatDoggos = require('../treat-doggos.js');
 
 describe('treatDoggos', () => {
-  it('', () => {
+  it('Input dog array with 1 dog object, return new array with dog object hasRabies key to false', () => {
     // first test here
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    expect(treatDoggos(dogs)).not.toBe(dogs);
+    expect(treatDoggos(dogs)).toEqual([{ name: 'Otis', age: 3, hasRabies: false }]);
+  });
+  it('Input dog array with 1 dog object, return new array with dog object hasRabies key to false', () => {
+    // first test here
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    const newDogs = treatDoggos(dogs);
+    expect(newDogs !== dogs).toBe(true);
   });
 });
--- week_1_katas/treat-doggos.js
+++ week_1_katas/treat-doggos.js
@@ -1,3 +1,11 @@
-const treatDoggos = (dogs) => {};
+const treatDoggos = (dogs) => {
+
+    const newDogArray = dogs.map((dog)=>{
+        dog.hasRabies = false;
+        return dog;
+    });
+
+    return newDogArray;
+};
 
 module.exports = treatDoggos;
