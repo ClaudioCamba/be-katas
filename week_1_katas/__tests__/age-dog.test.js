673a15867c6d4b1b4a2f52620f6150ec9d5c884f
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-07 13:16:31 +0100

    Backend week 1 age-dog kata solution

--- week_1_katas/__tests__/age-dog.test.js
+++ week_1_katas/__tests__/age-dog.test.js
@@ -1,7 +1,23 @@
 const ageDog = require('../age-dog.js');
 
 describe('ageDog', () => {
-  it('', () => {
+  it("Input of { name: 'Otis', age: 1 } should return the same object with age equal to 2", () => {
     // first test here
+    const dog = { name: 'Otis', age: 1 };
+    expect(ageDog(dog, 1)).not.toBe(dog);
+    expect(ageDog(dog, 1)).toEqual({ name: 'Otis', age: 2 });
+  });
+  it("Input of { name: 'Otis', age: 1 } should return the same object with age equal to 6", () => {
+    // first test here
+    const dog = { name: 'Otis', age: 1 };
+    expect(ageDog(dog, 5)).toEqual({ name: 'Otis', age: 6 });
+    expect(ageDog(dog, 5)).toHaveProperty('name');
+    expect(ageDog(dog, 5)).toHaveProperty('age');
+  });
+  it("Input num of minus, undefined and null should return object without updates", () => {
+    // first test here
+    const dog = { name: 'Otis', age: 1 };
+    expect(ageDog(dog, -1)).not.toBe(dog);
+    expect(ageDog(dog, -1)).toEqual({ name: 'Otis', age: 1 });
   });
 });
--- week_1_katas/age-dog.js
+++ week_1_katas/age-dog.js
@@ -1,3 +1,12 @@
-const ageDog = (dog, num) => {};
+const ageDog = (dog, num) => {
+    const agedDog = {...dog};
+
+    if (num === undefined || num <= 0 || num === null){
+        return agedDog;
+    }
+    
+    agedDog.age += num;
+    return agedDog;
+};
 
 module.exports = ageDog;
