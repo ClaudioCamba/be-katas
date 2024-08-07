d671e555bf3c1338a0094dd6ff4b9f8c1b5386be
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-07 14:35:43 +0100

    Backend week 1 create-ref kata solution

--- week_1_katas/__tests__/create-ref.test.js
+++ week_1_katas/__tests__/create-ref.test.js
@@ -1,7 +1,36 @@
 const createRef = require('../create-ref.js');
+const people = [
+  { name: 'vel', phoneNumber: '01134445566', address: 'Northcoders, Leeds' },
+  {
+    name: 'ant',
+    phoneNumber: '01612223344',
+    address: 'Northcoders, Manchester'
+  },
+  { name: 'mitch', phoneNumber: '07777777777', address: null }
+];
+const people2 = [
+  { name: 'vel', phoneNumber: '01134445566', address: 'Northcoders, Leeds' },
+  {
+    name: 'ant',
+    phoneNumber: '01612223344',
+    address: 'Northcoders, Manchester'
+  },
+  { name: 'mitch', phoneNumber: '07777777777', address: null }
+];
 
 describe('createRef', () => {
-  it('', () => {
+  it('Input an array with a single object should return an object with vel key and their phone number', () => {
     // first test here
+    const person = [{ name: 'vel', phoneNumber: '01134445566', address: 'Northcoders, Leeds' }];
+    expect(createRef(person)).toEqual({ vel:'01134445566' });
+  });
+  it('Input an array with multiple object should return an object with all the people and phone number', () => {
+    // first test here
+    expect(createRef(people)).toEqual({ vel:'01134445566', ant:'01612223344', mitch:'07777777777' });
+  });
+  it('The original array should not be mutated', () => {
+    // first test here
+    expect(createRef(people)).toEqual({ vel:'01134445566', ant:'01612223344', mitch:'07777777777' });
+    expect(people).toEqual(people2);
   });
 });
--- week_1_katas/create-ref.js
+++ week_1_katas/create-ref.js
@@ -1,3 +1,13 @@
-const createRef = () => {};
+const createRef = (peopleArray) => {
+    const people = {};
+    peopleArray.forEach(person => {
+        const name = person.hasOwnProperty('name');
+        const number = person.hasOwnProperty('phoneNumber');
+        if (name && number) people[person.name] = person.phoneNumber;
+    });
+
+    return people;
+};
 
 module.exports = createRef;
+ 
