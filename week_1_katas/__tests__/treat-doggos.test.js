3ca82e740d2fdad601e3531c1d2b4243ce437766
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-07 14:00:50 +0100

    update treat-doggos test

--- week_1_katas/__tests__/treat-doggos.test.js
+++ week_1_katas/__tests__/treat-doggos.test.js
@@ -8,9 +8,13 @@ describe('treatDoggos', () => {
     expect(treatDoggos(dogs)).toEqual([{ name: 'Otis', age: 3, hasRabies: false }]);
   });
   it('Input dog array with 1 dog object, return new array with dog object hasRabies key to false', () => {
-    // first test here
     const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
     const newDogs = treatDoggos(dogs);
     expect(newDogs !== dogs).toBe(true);
+  });
+  it('Input dog array with 2 dog object, return new array with dog object hasRabies key to false', () => {
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }, { name: 'Ted', age: 6, hasRabies: true }];
+    expect(treatDoggos(dogs)).not.toBe(dogs);
+    expect(treatDoggos(dogs)).toEqual(dogs);
   });
 });
