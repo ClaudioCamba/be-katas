daf404d3d1d4502dd92922d37c2b7a02e4b6f0d5
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-08 22:15:05 +0100

    Backend week 1 format-Albums kata solution

--- week_1_katas/__tests__/formatAlbums.test.js
+++ week_1_katas/__tests__/formatAlbums.test.js
@@ -1,7 +1,31 @@
 const formatAlbums = require('../format-albums.js');
 
+const albums = [
+  { name: 'Grammatics', artist: 'Grammatics', releaseYear: 2009 },
+  { name: 'Kingdom of Rust', artist: 'Doves', releaseYear: 2009 }
+];
+
+const artistIdReference = {
+  Grammatics: 9923,
+  Doves: 324
+};
+
 describe('formatAlbums', () => {
-  it('', () => {
+  it('Input albums  & artist ref variables and return array with objects containing correct artist id', () => {
     // first test here
+    expect(formatAlbums(albums, artistIdReference)).toEqual([
+      { name: 'Grammatics', artistId: 9923, releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artistId: 324, releaseYear: 2009 },
+    ]);
+
+    expect(albums).toEqual([
+      { name: 'Grammatics', artist: 'Grammatics', releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artist: 'Doves', releaseYear: 2009 }
+    ]);
+
+    expect(artistIdReference).toEqual({
+      Grammatics: 9923,
+      Doves: 324
+    });
   });
 });
--- week_1_katas/format-albums.js
+++ week_1_katas/format-albums.js
@@ -1,3 +1,21 @@
-const formatAlbums = () => {};
+const formatAlbums = (albums, artistIdReference) => {
+    const updatedAlbums = [];
+
+
+    albums.forEach(album => {
+        if (album.hasOwnProperty('artist')){
+            if (artistIdReference.hasOwnProperty(album.artist)){
+                updatedAlbums.push({
+                    name: album.name,
+                    artistId: artistIdReference[album.artist],
+                    releaseYear: album.releaseYear
+                })
+            }
+        }
+    });
+
+
+    return updatedAlbums;
+};
 
 module.exports = formatAlbums;
