47783a8db03e130d0aa4872d2594ab7b88b227af
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-09 11:54:01 +0100

    Backend week 2 supermarket-queue kata solution

--- week_2_katas/__tests__/supermarket-queue.test.js
+++ week_2_katas/__tests__/supermarket-queue.test.js
@@ -1,7 +1,26 @@
 const { supermarketQueue } = require('../supermarket-queue')
 
 describe('supermarketQueue()', () => {
-  it('', () => {
+  it('Input queue of [2, 2, 2], 1 should return 6 as there is only 1 till and 3 customers 2 min each', () => {
     // first test here
+    expect(supermarketQueue([2, 2, 2], 1)).toBe(6);
+  })
+  it('Input queue of [2, 10], 2 should return 10 as there are 2 tills and second customer will take longest', () => {
+    expect(supermarketQueue([2, 10], 2)).toBe(10);
+  })
+  it('Input queue of [2, 2, 2], 2 should return 4 as till 1 processess 2 customer', () => {
+    expect(supermarketQueue([2, 2, 2], 2)).toBe(4);
+  })
+  it('Input queue of [2, 3, 10], 2 should return 12', () => {
+    expect(supermarketQueue([2, 3, 10], 2)).toBe(12);
+  })
+  it('Input queue of [2, 3, 10], 3 should return 10', () => {
+    expect(supermarketQueue([2, 3, 10], 3)).toBe(10);
+  })
+  it('Input queue of [4, 6, 10], 4 should return 10', () => {
+    expect(supermarketQueue([4, 6, 10], 4)).toBe(10);
+  })
+  it('Input queue of [4, 6, 10, 6], 4 should return 10', () => {
+    expect(supermarketQueue([4, 6, 10, 6], 2)).toBe(14);
   })
 })
--- week_2_katas/supermarket-queue.js
+++ week_2_katas/supermarket-queue.js
@@ -1,6 +1,24 @@
 // Please do not change the name of this function
 const supermarketQueue = (customers, checkouts) => {
   // Your code here
+  if (checkouts === 1) return customers.reduce((acc, curr) => acc + curr);
+
+  const allCheckouts = [];
+  let pos = 0;
+
+  customers.forEach(cust => {
+    if (allCheckouts.length === 0 || allCheckouts.length < checkouts){
+      allCheckouts.push(cust);
+    } else {
+      allCheckouts[pos] += cust;
+    }
+    pos++;  
+
+    if (pos >= checkouts) pos = 0;
+  });
+
+  return Math.max(...allCheckouts);
+
 }
 
 module.exports = { supermarketQueue }
