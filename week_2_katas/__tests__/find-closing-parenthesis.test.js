60f981ec047d577ad171384c5081af58e45a4c32
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-08-09 18:22:42 +0100

    Backend week 1 find-closing-parenthesis kata solution

--- week_2_katas/__tests__/find-closing-parenthesis.test.js
+++ week_2_katas/__tests__/find-closing-parenthesis.test.js
@@ -1,7 +1,23 @@
 const { findClosingParenthesis } = require('../find-closing-parenthesis')
 
 describe('findClosingParenthesis()', () => {
-  it('', () => {
+  it('Empty string input should return -1', () => {
     // first test here
+    expect(findClosingParenthesis('')).toBe(-1);
+  })
+  it('Input values without number should return -1', () => {
+    expect(findClosingParenthesis('hello')).toBe(-1);
+  })
+  it('Input string with number 1 should return 6 as its the index of first closing parathesis', () => {
+    expect(findClosingParenthesis('(hello)', 1)).toBe(6);
+  })
+  it('Input string with number 2 should return 31 as its the index of 2nd closing parathesis', () => {
+    expect(findClosingParenthesis('Hello, (world). (Something Else).', 2)).toBe(31);
+  })
+  it('Input string with number 3 should return 35 as its the index of 3rd closing parathesis', () => {
+    expect(findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 3)).toBe(35);
+  })
+  it('Input string with number 1 should return 41 as its the index of 1st closing parathesis', () => {
+    expect(findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 1)).toBe(41);
   })
 })
--- week_2_katas/find-closing-parenthesis.js
+++ week_2_katas/find-closing-parenthesis.js
@@ -1,6 +1,27 @@
 // Please do not change the name of this function
-const findClosingParenthesis = (str) => {
+const findClosingParenthesis = (str, num) => {
   // Your code here
+  if (typeof str !== 'string' || str.length === 0) return -1;
+  if (typeof num !== 'number' || num === undefined) return -1;
+  
+  const string = str.split('');
+  let open = [], close = [], count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '('){
+      count++;
+      open.push(count);
+      close.unshift(count)
+    }
+    if (string[i] === ')'){
+      if (open[open.length-1] === num && close[0] === num){
+        return i;
+      }
+      open.pop();
+      close.shift();
+    }
+
+  }
 }
 
 module.exports = { findClosingParenthesis }
